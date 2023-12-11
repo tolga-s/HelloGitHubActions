8f8a33d973a33a8443e342f95da00ccd967ce415
Erik Hoelzel <erik.hoelzel@posteo.de>
2023-12-11 23:47:11 +0100

    adds Hello World stuff

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int count = 0;
+        while (true) {
+            Thread.sleep(2 * 1000);
+            System.out.println(String.format("%d seconds have passed ...", count++));
+        }
+    }
+}
